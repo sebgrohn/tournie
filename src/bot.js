2aae4dc446a4ad0e319800b3b99aaa3140e9c45d
Sebastian Gröhn <sebastian@sebgrohn.se>
2018-12-09 18:18:42 +0100

    Add whoami, login, logout commands

--- src/bot.js
+++ src/bot.js
@@ -5,20 +5,32 @@ const { formatTimestamp, formatDescription, formatGameName } = require('./format
 const slashCommand = '/challonge';
 const supportedCommands = [
     ['[list]', 'list open tournaments'],
+    ['whoami', 'show who you are on Challonge'],
+    ['login <challonge_username>', 'connect your Slack and Challonge accounts'],
+    ['logout', 'disconnect your Slack and Challonge accounts'],
     ['help|usage', 'show this information'],
 ];
 
 function botFactory(challongeService, userRepository) {
-    const { fetchOpenTournaments } = challongeService;
+    const { fetchOpenTournaments, fetchMembers } = challongeService;
 
     return async function handleMessage(message) {
         const { text } = message;
-        const command = text || 'list';
+        const command = (text || 'list').split(/\s+/)[0];
 
         try {
             switch (command) {
                 case 'list':
                     return await listTournaments(message);
+
+                case 'whoami':
+                    return await getCurrentUser(message);
+
+                case 'login':
+                    return await logInUser(message);
+
+                case 'logout':
+                    return await logOutUser(message);
 
                 case 'help':
                 case 'usage':
@@ -58,6 +70,41 @@ function botFactory(challongeService, userRepository) {
             new SlackTemplate('*:trophy: Open tournaments: :trophy:*'),
         )(openTournaments)
             .get();
+    }
+
+    async function getCurrentUser({ sender }) {
+        const user = await userRepository.getUser(sender);
+        if (!user) {
+            return 'I don\'t know who you are. :crying_cat_face:';
+        }
+        return `You are known as *${user.challongeUsername}* (${user.challongeEmailHash ? 'verified' : 'unverified'}). :ok_hand:`;
+    }
+
+    async function logInUser({ text, sender }) {
+        const user = await userRepository.getUser(sender);
+        if (user) {
+            return `You are already logged in as *${user.challongeUsername}* (${user.challongeEmailHash ? 'verified' : 'unverified'}). :angry:`;
+        }
+
+        const challongeUsername = text.split(/\s+/)[1];
+        if (!challongeUsername) {
+            return 'Your need to specify a Challonge username. :nerd_face:';
+        }
+
+        const challongeMembers = await fetchMembers();
+        const { email_hash: challongeEmailHash } = R.find(m => m.username === challongeUsername)(challongeMembers) || {};
+
+        await userRepository.addUser(sender, challongeUsername, challongeEmailHash);
+        return `Congrats! You are now known as *${challongeUsername}* (${challongeEmailHash ? 'verified' : 'unverified'}). :tada:`;
+    }
+
+    async function logOutUser({ sender }) {
+        const user = await userRepository.getUser(sender);
+        if (!user) {
+            return 'I don\'t know who you are. :crying_cat_face:';
+        }
+        await userRepository.deleteUser(sender);
+        return `Okay, you are now forgotten. I hope to see you later! :wave:`;
     }
 
     async function usage(message, invalidCommand) {
