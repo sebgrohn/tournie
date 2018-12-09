2b94fab2ca71a64823ff225c90292a169bf46f9e
Sebastian Gröhn <sebastian@sebgrohn.se>
2018-12-09 14:08:48 +0100

    Remove check for Slack slash message type

--- src/bot.js
+++ src/bot.js
@@ -2,11 +2,17 @@ const R = require('ramda');
 const SlackTemplate = require('claudia-bot-builder').slackTemplate;
 const { formatTimestamp, formatDescription, formatGameName } = require('./formatting');
 
+const slashCommand = '/challonge';
+const supportedCommands = [
+    ['[list]', 'list open tournaments'],
+    ['help|usage', 'show this information'],
+];
+
 function botFactory(challongeService) {
     const { fetchOpenTournaments } = challongeService;
 
     return async function handleMessage(message) {
-        const { type, text } = message;
+        const { text } = message;
         const command = text || 'list';
 
         try {
@@ -16,68 +22,62 @@ function botFactory(challongeService) {
 
                 case 'help':
                 case 'usage':
-                default:
                     return await usage(message);
+
+                default:
+                    return await usage(message, true);
             }
         } catch (error) {
             return handleError(message, error);
         }
     }
 
-    async function listTournaments({ type }) {
+    async function listTournaments(message) {
         const openTournaments = await fetchOpenTournaments();
 
-        if (type === 'slack-slash-command') {
-            return R.reduce(
-                (response, t) => {
+        return R.reduce(
+            (response, t) => {
+                response
+                    .addAttachment(`tournament-${t.subdomain}-${t.id}`)
+                    .addTitle(t.name, t.full_challonge_url)
+                    .addText(formatDescription(t.description))
+                    .addColor('#252830')
+                    .addField('Tournament', `${formatGameName(t.game_name)} – ${t.tournament_type}`, true)
+                    .addField('# Players', `${t.participants_count} / ${t.signup_cap}`, true);
+
+                if (t.started_at) {
+                    response.addField('Started', formatTimestamp(t.started_at), true);
+                } else {
                     response
-                        .addAttachment(`tournament-${t.subdomain}-${t.id}`)
-                        .addTitle(t.name, t.full_challonge_url)
-                        .addText(formatDescription(t.description))
-                        .addColor('#252830')
-                        .addField('Tournament', `${formatGameName(t.game_name)} – ${t.tournament_type}`, true)
-                        .addField('# Players', `${t.participants_count} / ${t.signup_cap}`, true);
+                        .addField('Created', formatTimestamp(t.created_at), true)
+                        .addLinkButton('Sign Up', t.sign_up_url);
+                }
 
-                    if (t.started_at) {
-                        response.addField('Started', formatTimestamp(t.started_at), true);
-                    } else {
-                        response
-                            .addField('Created', formatTimestamp(t.created_at), true)
-                            .addLinkButton('Sign Up', t.sign_up_url);
-                    }
-
-                    return response.addField('State', `${t.state} (${t.progress_meter}%)`, true);
-                },
-                new SlackTemplate('*:trophy: Open tournaments: :trophy:*'),
-            )(openTournaments)
-                .get();
-        } else {
-            return `Open tournaments:\n${JSON.stringify(openTournaments)}`;
-        }
+                return response.addField('State', `${t.state} (${t.progress_meter}%)`, true);
+            },
+            new SlackTemplate('*:trophy: Open tournaments: :trophy:*'),
+        )(openTournaments)
+            .get();
     }
 
-    async function usage({ type }) {
-        if (type === 'slack-slash-command') {
-            return new SlackTemplate(':trophy: This is not how you win a game...')
-                .addAttachment('usage')
-                .addText('Supported commands:\n• `/challonge [list]` to list open tournaments\n• `/challonge help|usage` to show this information')
-                .addColor('#252830')
-                .get();
-        } else {
-            return 'Supported commands:\n* /challonge [list] to list open tournaments\n* /challonge help|usage to show this information';
-        }
+    async function usage(message, invalidCommand) {
+        const supportedCommandsString = R.pipe(
+            R.map(([c, d]) => `• \`${slashCommand} ${c}\` to ${d}`),
+            R.join('\n')
+        )(supportedCommands);
+
+        return new SlackTemplate(invalidCommand ? ':trophy: This is not how you win a game...' : undefined)
+            .addAttachment('usage')
+            .addText(`Supported commands:\n${supportedCommandsString}`)
+            .addColor('#252830')
+            .get();
     }
 
-    function handleError({ type }, { response, request, message }) {
+    function handleError(_, { response, message }) {
         const errorMessage = response
-                && `${response.status} – ${JSON.stringify(response.data)}` 
+                && `${response.status} – ${JSON.stringify(response.data)}`
             || message;
-
-        if (type === 'slack-slash-command') {
-            return new SlackTemplate(`Received error from Challonge API: :crying_cat_face:\n${errorMessage}`).get();
-        } else {
-            return `Received error from Challonge API: ${errorMessage}`;
-        }
+        return `:crying_cat_face: There was an error: ${errorMessage}`;
     }
 };
 
