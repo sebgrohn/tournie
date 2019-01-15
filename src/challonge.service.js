02dc7a5c1ef528d08eeaeaaedc5583a64df57e56
tropy <isak.eriksson@acino.se>
2019-01-15 23:56:48 +0100

    added a mock api, happy path tests for all service funcs, changed dep injection style

--- src/challonge.service.js
+++ src/challonge.service.js
@@ -1,121 +1,119 @@
 const R = require('ramda');
 
-function challongeServiceFactory({ api, organization }) {
-    async function fetchAllTournaments() {
-        const { data } = await api.get('tournaments.json', {
-            params: {
-                subdomain: organization,
-            },
-        });
-        return R.map(({ tournament }) => tournament)(data);
-    }
+const fetchAllTournaments = ({ api, organization }) => async function () {
+    const { data } = await api.get('tournaments.json', {
+        params: {
+            subdomain: organization,
+        },
+    });
+    return R.map(({ tournament }) => tournament)(data);
+}
 
-    async function fetchOpenTournaments() {
-        const { data } = await api.get('tournaments.json', {
-            params: {
-                subdomain: organization,
-                // state: '...', // one of 'all', 'pending', 'in_progress', 'ended'
-            },
-        });
-        return R.pipe(
-            R.map(({ tournament }) => tournament),
-            R.filter(t => ['pending', 'underway'].includes(t.state)),
-        )(data);
-    }
+const fetchOpenTournaments = ({ api, organization }) => async function () {
+    const { data } = await api.get('tournaments.json', {
+        params: {
+            subdomain: organization,
+            // state: '...', // one of 'all', 'pending', 'in_progress', 'ended'
+        },
+    });
+    return R.pipe(
+        R.map(({ tournament }) => tournament),
+        R.filter(t => ['pending', 'underway'].includes(t.state)),
+    )(data);
+}
 
-    async function fetchTournament(tournamentId) {
-        const { data } = await api.get(`tournaments/${tournamentId}.json`, {
-            params: {
-                include_participants: 1,
-                include_matches: 1,
-            }
-        });
-        const { tournament } = data;
-        return {
-            ...tournament,
-            participants: R.map(({ participant }) => participant)(tournament.participants),
-            matches: R.map(({ match }) => match)(tournament.matches),
-        };
-    }
-
-    async function fetchTournamentParticipants(tournamentId) {
-        const { data } = await api.get(`tournaments/${tournamentId}/participants.json`);
-        return R.map(({ participant }) => participant)(data);
-    }
-
-    async function fetchMembers() {
-        const tournaments = await fetchAllTournaments();
-
-        const participantsPromises = R.pipe(
-            R.take(5),
-            R.map(({ id }) => id),
-            R.map(fetchTournamentParticipants),
-        )(tournaments);
-        const participants = await Promise.all(participantsPromises);
-
-        return R.pipe(
-            R.unnest,
-            R.uniqBy(({ email_hash }) => email_hash),
-            R.map(R.pick(['username', 'email_hash', 'challonge_email_address_verified'])),
-        )(participants);
-    }
-
-    async function fetchOpenMatchesForMember(memberEmailHash) {
-        const tournaments = await fetchOpenTournaments();
-
-        const tournamentsDetailsPromises = R.pipe(
-            R.map(({ id }) => id),
-            R.map(fetchTournament),
-        )(tournaments);
-        const tournamentsDetails = await Promise.all(tournamentsDetailsPromises);
-
-        const tournamentsById = R.pipe(
-            R.map(t => [t.id, t]),
-            R.fromPairs,
-        )(tournaments);
-
-        const participantsById = R.pipe(
-            R.map(({ participants }) => participants),
-            R.unnest,
-            R.map(p => [p.id, p]),
-            R.fromPairs,
-        )(tournamentsDetails);
-
-        return R.pipe(
-            R.map(({ matches }) => matches),
-            R.unnest,
-            R.filter(m => ['pending', 'open'].includes(m.state)),
-            R.map(m => ({
-                ...m,
-                tournament: tournamentsById[m.tournament_id],
-                player1: m.player1_id ? participantsById[m.player1_id] : null,
-                player2: m.player2_id ? participantsById[m.player2_id] : null,
-            })),
-            R.filter(({ player1, player2 }) =>
-                player1 && player1.email_hash === memberEmailHash
-                    || player2 && player2.email_hash === memberEmailHash),
-        )(tournamentsDetails);
-    }
-
-    async function addTournamentParticipant(tournamentId, memberUsername, participantName = undefined) {
-        const { data } = await api.post(`tournaments/${tournamentId}/participants.json`, {
-            participant: {
-                challonge_username: memberUsername,
-                name: participantName,
-            },
-        });
-        return data.participant;
-    }
-
+const fetchTournament = ({ api }) => async function (tournamentId) {
+    const { data } = await api.get(`tournaments/${tournamentId}.json`, {
+        params: {
+            include_participants: 1,
+            include_matches: 1,
+        }
+    });
+    const { tournament } = data;
     return {
-        fetchAllTournaments,
-        fetchOpenTournaments,
-        fetchTournament,
-        fetchTournamentParticipants,
-        fetchMembers,
-        fetchOpenMatchesForMember,
-        addTournamentParticipant,
+        ...tournament,
+        participants: R.map(({ participant }) => participant)(tournament.participants),
+        matches: R.map(({ match }) => match)(tournament.matches),
     };
 }
 
-module.exports = challongeServiceFactory;
+const fetchTournamentParticipants = ({ api }) => async function (tournamentId) {
+    const { data } = await api.get(`tournaments/${tournamentId}/participants.json`);
+    return R.map(({ participant }) => participant)(data);
+}
+
+const fetchMembers = ({ api, organization }) => async function () {
+    const tournaments = await fetchAllTournaments({ api, organization })();
+
+    const participantsPromises = R.pipe(
+        R.take(5),
+        R.map(({ id }) => id),
+        R.map(fetchTournamentParticipants({ api })),
+    )(tournaments);
+    const participants = await Promise.all(participantsPromises);
+
+    return R.pipe(
+        R.unnest,
+        R.uniqBy(({ email_hash }) => email_hash),
+        R.map(R.pick(['username', 'email_hash', 'challonge_email_address_verified'])),
+    )(participants);
+}
+
+const fetchOpenMatchesForMember = ({ api, organization }) => async function (memberEmailHash) {
+    const tournaments = await fetchOpenTournaments({ api, organization })();
+
+    const tournamentsDetailsPromises = R.pipe(
+        R.map(({ id }) => id),
+        R.map(fetchTournament({ api })),
+    )(tournaments);
+    const tournamentsDetails = await Promise.all(tournamentsDetailsPromises);
+
+    const tournamentsById = R.pipe(
+        R.map(t => [t.id, t]),
+        R.fromPairs,
+    )(tournaments);
+
+    const participantsById = R.pipe(
+        R.map(({ participants }) => participants),
+        R.unnest,
+        R.map(p => [p.id, p]),
+        R.fromPairs,
+    )(tournamentsDetails);
+
+    return R.pipe(
+        R.map(({ matches }) => matches),
+        R.unnest,
+        R.filter(m => ['pending', 'open'].includes(m.state)),
+        R.map(m => ({
+            ...m,
+            tournament: tournamentsById[m.tournament_id],
+            player1: m.player1_id ? participantsById[m.player1_id] : null,
+            player2: m.player2_id ? participantsById[m.player2_id] : null,
+        })),
+        R.filter(({ player1, player2 }) =>
+            player1 && player1.email_hash === memberEmailHash
+                || player2 && player2.email_hash === memberEmailHash),
+    )(tournamentsDetails);
+}
+
+const addTournamentParticipant = ({ api }) => async function (tournamentId, memberUsername, participantName = undefined) {
+    const { data } = await api.post(`tournaments/${tournamentId}/participants.json`, {
+        participant: {
+            challonge_username: memberUsername,
+            name: participantName,
+        },
+    });
+    return data.participant;
+}
+
+const challongeService = {
+    fetchAllTournaments,
+    fetchOpenTournaments,
+    fetchTournament,
+    fetchTournamentParticipants,
+    fetchMembers,
+    fetchOpenMatchesForMember,
+    addTournamentParticipant,
+}
+
+module.exports = challongeService;
--- src/challonge.service.test.js
+++ src/challonge.service.test.js
@@ -1,24 +1,201 @@
-const challongeServiceFactory = require('./challonge.service');
+const challongeService = require('./challonge.service');
+const R = require('ramda');
+const memberView = R.pick(['username', 'email_hash', 'challonge_email_address_verified']);
 
-test('fetchAllTournaments', async () => {
-    const mockFn = jest.fn();
-    const api = {
-        get: async (route, query) => {
-            mockFn(route, query);
-            return { 
+const organization = 'company'; // use the same org for all tests...
+
+const p1 = { id: 'p1', username: 'username1', email_hash: 'email_hash1', challonge_email_address_verified: 'challonge_email_address_verified1' };
+const p2 = { id: 'p2', username: 'username2', email_hash: 'email_hash2', challonge_email_address_verified: 'challonge_email_address_verified2' };
+const p3 = { id: 'p3', username: 'username3', email_hash: 'email_hash3', challonge_email_address_verified: 'challonge_email_address_verified3' };
+const t1 = {
+  id: 't1',
+  state: 'underway',
+  participants: [ { participant: p1 }, { participant: p2 } ],
+  matches: [ { match: { state: 'open', player1_id: 'p1', player2_id: 'p2' } } ]
+}
+const t2 = { id: 't2', state: 'not open', participants: [], matches: [] }
+const mockApi = spyFunc => ({
+  get: async (route, query) => {
+      spyFunc(route, query);
+      let res;
+      switch (route) {
+        case 'tournaments.json':
+            res = {
                 data: [
-                    { tournament: '10' }, 
-                    { tournament: 'a' },
+                    { tournament: t1 },
+                    { tournament: t2 },
                 ],
             };
-        },
-    };
-    const organization = 'company';
-    const tournaments = await challongeServiceFactory({ api, organization })
-        .fetchAllTournaments();
+            break;
+        case 'tournaments/t1.json':
+            res = {
+                data: { tournament: t1 }
+            };
+            break;
+        case 'tournaments/t2.json':
+            res = {
+                data: { tournament: t2 }
+            };
+            break;
+        case 'tournaments/t1/participants.json':
+            res = {
+                data: [
+                    { participant: p1 },
+                    { participant: p2 },
+                ],
+            };
+            break;
+        case 'tournaments/t2/participants.json':
+            res = {
+                data: [
+                    { participant: p1 },
+                    { participant: p3 },
+                ],
+            };
+            break;
+        default:
 
-    expect(mockFn.mock.calls.length).toBe(1);
-    expect(mockFn.mock.calls[0])
+      }
+      return res;
+  },
+  post: async (route, query) => {
+      spyFunc(route, query);
+      const res = {
+          data: { participant: query }
+      };
+      return res;
+  },
+});
+
+test('fetchAllTournaments', async () => {
+    const spyFunc = jest.fn();
+    const api = mockApi(spyFunc);
+    const tournaments = await challongeService
+        .fetchAllTournaments({ api, organization })();
+
+    expect(spyFunc.mock.calls.length).toBe(1);
+    expect(spyFunc.mock.calls[0])
         .toEqual(['tournaments.json', { params: { subdomain: 'company' } }]);
-    expect(tournaments).toEqual(['10', 'a']);
+    expect(tournaments).toEqual([t1, t2]);
+});
+
+test('fetchOpenTournaments', async () => {
+    const spyFunc = jest.fn();
+    const api = mockApi(spyFunc);
+    const tournaments = await challongeService
+        .fetchOpenTournaments({ api, organization })();
+
+    expect(spyFunc.mock.calls.length).toBe(1);
+    expect(spyFunc.mock.calls[0])
+        .toEqual(['tournaments.json', { params: { subdomain: 'company' } }]);
+    expect(tournaments).toEqual([t1]);
+});
+
+test('fetchTournament', async () => {
+    const tournamentId = 't2';
+    const spyFunc = jest.fn();
+    const api = mockApi(spyFunc);
+    const tournament = await challongeService
+        .fetchTournament({ api, organization })(tournamentId);
+
+    expect(spyFunc.mock.calls.length).toBe(1);
+    expect(spyFunc.mock.calls[0])
+        .toEqual(['tournaments/t2.json', { params: { include_matches: 1, include_participants: 1 } }]);
+    expect(tournament).toEqual(t2);
+});
+
+test('fetchTournamentParticipants', async () => {
+    const tournamentId = 't1';
+    const spyFunc = jest.fn();
+    const api = mockApi(spyFunc);
+    const tournaments = await challongeService
+        .fetchTournamentParticipants({ api, organization })(tournamentId);
+
+    expect(spyFunc.mock.calls.length).toBe(1);
+    expect(spyFunc.mock.calls[0])
+        .toEqual(['tournaments/t1/participants.json', undefined]);
+    expect(tournaments).toEqual([p1, p2]);
+});
+
+test('fetchMembers', async () => {
+    const spyFunc = jest.fn();
+    const api = mockApi(spyFunc);
+    const tournaments = await challongeService
+        .fetchMembers({ api, organization })();
+
+    expect(spyFunc.mock.calls.length).toBe(3);
+    expect(spyFunc.mock.calls[0])
+        .toEqual(['tournaments.json', { params: { subdomain: 'company' } }]);
+    expect(spyFunc.mock.calls[1])
+        .toEqual(['tournaments/t1/participants.json', undefined]);
+    expect(spyFunc.mock.calls[2])
+        .toEqual(['tournaments/t2/participants.json', undefined]);
+    expect(tournaments).toEqual([
+      memberView(p1),
+      memberView(p2),
+      memberView(p3)
+    ]);
+});
+
+test('fetchOpenMatchesForMember', async () => {
+    const memberEmailHash = 'email_hash2';
+    const spyFunc = jest.fn();
+    const api = mockApi(spyFunc);
+    const matches = await challongeService
+        .fetchOpenMatchesForMember({ api, organization })(memberEmailHash);
+
+    expect(spyFunc.mock.calls.length).toBe(2);
+    expect(spyFunc.mock.calls[0])
+        .toEqual(['tournaments.json', { params: { subdomain: 'company' } }]);
+    expect(spyFunc.mock.calls[1])
+        .toEqual(['tournaments/t1.json', { params: { include_matches: 1, include_participants: 1 } }]);
+    expect(matches).toEqual([
+      {
+         player1: {
+           challonge_email_address_verified: 'challonge_email_address_verified1',
+           email_hash: 'email_hash1',
+           id: 'p1',
+           username: 'username1'
+         },
+         player1_id: 'p1',
+         player2: {
+           challonge_email_address_verified: 'challonge_email_address_verified2',
+           email_hash: 'email_hash2',
+           id: 'p2',
+           username: 'username2'
+         },
+         player2_id: 'p2',
+         state: 'open',
+         tournament: undefined
+       }
+    ]);
+});
+
+test('addTournamentParticipant', async () => {
+    const spyFunc = jest.fn();
+    const api = mockApi(spyFunc);
+    const tournaments = await challongeService
+        .addTournamentParticipant({ api, organization })(
+          'p4',
+          'memberUsername',
+          'participantName'
+        );
+
+    expect(spyFunc.mock.calls.length).toBe(1);
+    expect(spyFunc.mock.calls[0])
+        .toEqual([
+            'tournaments/p4/participants.json',
+            {
+                participant: {
+                    challonge_username: 'memberUsername',
+                    name: 'participantName',
+                }
+            }
+        ]);
+    expect(tournaments).toEqual({
+        participant: {
+            challonge_username: 'memberUsername',
+            name: 'participantName',
+        }
+    });
 });
