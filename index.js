3ecc69060d1212f157a5ca0a2aa6fba9febdd31d
Sebastian Gröhn <sebastian@sebgrohn.se>
2019-01-27 17:02:23 +0100

    WIP Send delayed responses

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { SimpleDB } = require('aws-sdk');
+const { SimpleDB, Lambda } = require('aws-sdk');
 const R = require('ramda');
 const axios = require('axios');
 const botBuilder = require('claudia-bot-builder');
+const slackDelayedReply = botBuilder.slackDelayedReply;
 const botFactory = require('./src/bot');
 // eslint-disable-next-line node/no-missing-require
 const { apiKey, organization } = require('./challonge');
@@ -17,6 +18,7 @@ const api = axios.create({
     },
 });
 const simpleDb = new SimpleDB({ region });
+const lambda = new Lambda({ region });
 
 const challongeService =  challongeServiceFactory({ api, organization });
 const userRepository = userRepositoryFactory(simpleDb);
@@ -26,4 +28,37 @@ const bot = botFactory(handlers);
 // TODO this is an async operation; we don't know now if things go wrong...
 userRepository.initialize();
 
-module.exports = botBuilder(bot, { platforms: ['slackSlashCommand'] });
+async function delayedBot(message, apiRequest) {
+    // TODO should message action callbacks also be delayed?
+    await lambda.invoke({
+        FunctionName: apiRequest.lambdaContext.functionName,
+        InvocationType: 'Event',
+        Payload: JSON.stringify({
+            slackEvent: message,
+        }),
+        Qualifier: apiRequest.lambdaContext.functionVersion,
+    }).promise();
+    // TODO What to return here to not get duplicate responses (immediate + delayed)? I have tried
+    //      * undefined => "{}",
+    //      * false => "false",
+    //      * true => ?,
+    //      * {} => "{}", and
+    //      * '' => Slack error.
+    //      replaceOriginal on the delayed response doesn't have any effect.
+    return {};
+}
+
+const botApi = botBuilder(delayedBot, { platforms: ['slackSlashCommand'] });
+
+botApi.intercept(async (event) => {
+    const { slackEvent: message } = event;
+    if (!message) {
+        // if this is a normal web request, let it run
+        return event;
+    }
+
+    await slackDelayedReply(message, await bot(message));
+    return false;
+});
+
+module.exports = botApi;
